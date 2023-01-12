eb3299c6892f4aa8d6d5d3d8e0b8e0b72e63852d
Jahangir Alam <samraatjahangir@gmail.com>
2023-01-13 01:49:44 +0600

    React Native Practice

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+} from "react-native";
+import Footer from "./components/Footer";
+import ListViewComponent from "./components/ListViewComponent";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Jahangir</Text>
+        <StatusBar style="auto" />
+        <Text>Some text</Text>
+        <View>
+          <Text>Some more text</Text>
+        <Image source={require("./assets/jahangir.jpg")} style={{ width: 200, height: 200, marginBottom: 20 }} />
+        </View>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+          }}
+          defaultValue="You can type in me Here"
+        />
+        <Text>Hello, I am your cat!</Text>
+      </View>
+      {/* <ListViewComponent /> */}
+      <Footer />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+} from "react-native";
+
+const Footer = () => {
+  return (
+    <View style={{ flex: 1 }}>
+        <Text style={{ marginTop: 500, textAlign: "center", fontSize: 20, marginBottom: 10  }}>Software Developer Jahangir</Text>
+    </View>
+  );
+};
+
+export default Footer;
